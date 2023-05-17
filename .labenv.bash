56e0c375701265210e238cba1d7d080d3a0216b7
Javier Cañadillas <javiercm@google.com>
2023-05-17 18:02:29 +0100

    Removing unnecessary steps

--- .labenv.bash
+++ .labenv.bash
@@ -10,7 +10,7 @@ shopt -s direxpand
 ## Set desired environment variables
 # GCP Project ID configuraiton
 PROJECT_ID=YOUR_PROJECT_ID
-gcloud config set project $PROJECT_ID 2> /dev/null
+gcloud config set project $PROJECT_ID --no-user-output-enabled 2> /dev/null
 export PROJECT_ID
 
 ## Unconmment sections that you want enabled, replacing tags YOUR_* with
@@ -18,12 +18,8 @@ export PROJECT_ID
 #GCP Region configuration
 REGION=YOUR_REGION
 export REGION
-gcloud config set compute/region $REGION 2> /dev/null
-gcloud config set run/region $REGION 2> /dev/null
-
-# Code working directory configuration
-#WORKDIR=YOUR_WORKDIR
-#export WORKDIR
+gcloud config set compute/region $REGION --no-user-output-enabled 2> /dev/null
+gcloud config set run/region $REGION --no-user-output-enabled 2> /dev/null
 
 # Enable kubectl autocompletion and use kubectl alias
 #shellcheck disable=SC1090
@@ -31,13 +27,5 @@ source <(kubectl completion bash)
 alias k=kubectl
 complete -o default -F __start_kubectl k
 
-
-# Mark the sourcing of this file
-mkdir -p YOUR_CONFIG_DIR
-touch "YOUR_CONFIG_DIR/.labenv_sourced"
-
 # Set path for extra binaries and scripts
 [[ -d "$HOME/bin" ]] && export PATH="$HOME/bin:$PATH"
-
-# Rehash bash
-hash -r
